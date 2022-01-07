20fd4370b48f8f12ce5ae2d70ef3319e4b79baf7
Kavindu Lakshitha <Kavindulakshitha84@gmail.com>
2022-01-07 23:48:58 +0530

    Add Sample code to JS

--- FinalProject01.js
+++ FinalProject01.js
@@ -1,5 +1,12 @@
-funtion
-{
-    var name = "Kavindu";
-    prompt("Please Enter Your Name : " + name);
+
+var Name="Kavindu";
+prompt("Tell me your Name ","Kavindu");
+alert("Your Name is : "+Name);
+var num1 = 10;
+var num2= 20;
+var num3 =num1+num2;
+document.write("The Addition of Numbers = "+num3);
+function displayMsg(){
+ Alert("Javascript is Fun!");
 }
+confirm("Are you sure you want to exit?");

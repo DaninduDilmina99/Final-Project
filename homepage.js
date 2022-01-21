d41666f56d1a7253e16a16fb88b0c573c8a8eae6
Danindu Dilmina Leewanage <95994194+DaninduDilmina99@users.noreply.github.com>
2022-01-22 02:08:16 +0530

    navebar done

--- homepage.js
+++ homepage.js
@@ -3,3 +3,11 @@ let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.add('active');
 }
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+};

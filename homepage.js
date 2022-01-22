07897baf913436239ce93abc980aac75795056a0
Danindu Dilmina Leewanage <95994194+DaninduDilmina99@users.noreply.github.com>
2022-01-23 02:52:10 +0530

    swiper done

--- homepage.js
+++ homepage.js
@@ -11,3 +11,12 @@ document.querySelector('#nav-close').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 };
+
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});

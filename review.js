9ddd7e9d897e6e49c74f076e20e65b246f8e73dc
Danindu Dilmina Leewanage <95994194+DaninduDilmina99@users.noreply.github.com>
2022-01-30 21:48:28 +0530

    new update

--- review.js
+++ review.js
@@ -22,25 +22,26 @@ var swiper = new Swiper(".home-slider", {
 });
 
 var swiper = new Swiper(".review-slider", {
-    loop:true, 
-    grabCursor:true,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-    },
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+  },
 });
+
 
 
 // hariyata weda karanne nee. apahu compaare karala balanna

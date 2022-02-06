c1b99fbe6736c90c04844c9e6e3ced73c0020a2a
Kavindu Lakshitha <Kavindulakshitha84@gmail.com>
2022-02-06 20:06:52 +0530

    Half way done we must to do CSS part for desgining purposes

--- Login.php
+++ Login.php
@@ -1,23 +1,27 @@
 <?php
 
-include 'server.php';
+include 'config.php';
 
-session_start();
+
 
 error_reporting(0);
 
 if (isset($_SESSION["user_id"])) {
-  header("Location: HomePage.php");
+  header("Location: welcome.php");
 }
 
 if (isset($_POST["signup"])) {
   $full_name = mysqli_real_escape_string($conn, $_POST["signup_full_name"]);
   $email = mysqli_real_escape_string($conn, $_POST["signup_email"]);
   $password = mysqli_real_escape_string($conn, md5($_POST["signup_password"]));
- 
+  $cpassword = mysqli_real_escape_string($conn, md5($_POST["signup_cpassword"]));
+  $token = md5(rand());
 
   $check_email = mysqli_num_rows(mysqli_query($conn, "SELECT email FROM users WHERE email='$email'"));
- if ($check_email > 0) {
+
+  if ($password !== $cpassword) {
+    echo "<script>alert('Password did not match.');</script>";
+  } elseif ($check_email > 0) {
     echo "<script>alert('Email already exists in out database.');</script>";
   } else {
     $sql = "INSERT INTO users (full_name, email, password) VALUES ('$full_name', '$email', '$password')";
@@ -26,6 +30,23 @@ if (isset($_POST["signup"])) {
       $_POST["signup_full_name"] = "";
       $_POST["signup_email"] = "";
       $_POST["signup_password"] = "";
+      $_POST["signup_cpassword"] = "";
+
+      $to = $email;
+      
+
+      
+
+      // Always set content-type when sending HTML email
+      $headers = "MIME-Version: 1.0" . "\r\n";
+      $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+      // More headers
+      $headers .= "From: ". $my_email;
+
+     
+    } else {
+      echo "<script>alert('User registration failed.');</script>";
     }
   }
 }
@@ -39,7 +60,7 @@ if (isset($_POST["signin"])) {
   if (mysqli_num_rows($check_email) > 0) {
     $row = mysqli_fetch_assoc($check_email);
     $_SESSION["user_id"] = $row['id'];
-    header("Location: HomePage.php");
+    header("Location: homepage.php");
   } else {
     echo "<script>alert('Login details is incorrect. Please try again.');</script>";
   }
@@ -49,119 +70,84 @@ if (isset($_POST["signin"])) {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/83325a7e2a.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Login.css" />
-    <title>Sign in & Sign up Form</title>
-    
-  </head>
-  <body>
-    
-    <div class="container"> 
-      <div class="forms-container">    
-        <div class="signin-signup">
-          <form action="Login.php" class="sign-in-form" method ="POST">
-          <?php include('errors.php'); ?>
-            <div class="logo">
-              <img src="img/Logo.jpeg" alt="Logo"/>
-           </div>  
-            <h2 class="title">Sign in</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Email Address" name="email" value="<?php echo $_POST['email']; ?>" required/>
-            </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required />
-            </div>
-            <input type="submit" value="Login" class="btn solid" />
-            <p class="social-text">Or Sign in with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
-          <form action="Login.php" class="sign-up-form" method="POST">
-          <?php include('errors.php'); ?>
-            <div class="logo">
-              <img src="img/Logo.jpeg" alt="Logo"/>
-           </div>  
-            <h2 class="title">Sign up</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Full Name" name="signup_full_name" value="<?php echo $_POST["signup_full_name"]; ?>" required />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email Address" name="signup_email" value="<?php echo $_POST["signup_email"]; ?>" required/>
-            </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" name="password"/>
-            </div>
-            <input type="submit" class="btn" value="Sign up" name="Users"/>
-            <p class="social-text">Or Sign up with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
-        </div>
-      </div>
 
-      <div class="panels-container">
-        <div class="panel left-panel">
-          <div class="content">
-            <h3>Create an Account</h3>
-            <p>
-              Create your own account it takes less than a minute.
-            </p>
-            <button class="btn transparent" id="sign-up-btn">
-              Sign up
-            </button>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <title>Sign in & Sign up Form</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="forms-container">
+      <div class="signin-signup">
+        <form action="" method="post" class="sign-in-form">
+          <h2 class="title">Sign in</h2>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Email Address" name="email" value="<?php echo $_POST['email']; ?>" required />
           </div>
-          <img src="undraw_bus_stop_re_h8ej.svg" class="image" alt="" />
-
-        </div>
-        <div class="panel right-panel">
-          <div class="content">
-            <h3>One of us ?</h3>
-            <p>
-              If you already have an Account!
-            </p>
-            <button class="btn transparent" id="sign-in-btn">
-              Sign in
-            </button>
-          </div> 
-
-          <img src="undraw_medicine_b-1-ol.svg" class="image" alt="" />
-
-        </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required />
+          </div>
+          <input type="submit" value="Login" name="signin" class="btn solid" />
+ 
+        </form>
+        <form action="" class="sign-up-form" method="post">
+          <h2 class="title">Sign up</h2>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Full Name" name="signup_full_name" value="<?php echo $_POST["signup_full_name"]; ?>" required />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-envelope"></i>
+            <input type="email" placeholder="Email Address" name="signup_email" value="<?php echo $_POST["signup_email"]; ?>" required />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" name="signup_password" value="<?php echo $_POST["signup_password"]; ?>" required />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Confirm Password" name="signup_cpassword" value="<?php echo $_POST["signup_cpassword"]; ?>" required />
+          </div>
+          <input type="submit" class="btn" name="signup" value="Sign up" />
+        </form>
       </div>
     </div>
 
-    <script src="Login.js"></script>
-  </body>
+    <div class="panels-container">
+      <div class="panel left-panel">
+        <div class="content">
+          <h3>Create an Account</h3>
+          <p>
+          Create your own account it takes less than a minute.
+          </p>
+          <button class="btn transparent" id="sign-up-btn">
+            Sign up
+          </button>
+        </div>
+        <img src="img/medicine.svg" class="image" alt="" />
+      </div>
+      <div class="panel right-panel">
+        <div class="content">
+          <h3>One of us ?</h3>
+          <p>
+          If you already have an Account!
+          </p>
+          <button class="btn transparent" id="sign-in-btn">
+            Sign in
+          </button>
+        </div>
+        <img src="img/bus.svg" class="image" alt="" />
+      </div>
+    </div>
+  </div>
+
+  <script src="https://kit.fontawesome.com/83325a7e2a.js" crossorigin="anonymous"></script>
+  <script src="app.js"></script>
+</body>
+
 </html>
---
+++ navbar.php
@@ -0,0 +1,92 @@
+<header class="top">
+
+                  <div class="fixedArea">
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12 noPadding">
+                            <div class="content-wrapper one">
+                                
+                                <header class="header">
+                                    <nav class="navbar navbar-default myNavBar">
+                                        <div class="container">
+
+                                           
+                                            <div class="navbar-header">
+                                                <div class="row">
+                                                    <div class="col-md-9 col-sm-9 col-xs-9">
+                                                        <div class="row">
+                                                            <div class="col-md-3 col-xs-3 col-sm-3">
+                                                                <a style="padding-top:0px;" class=" navbar-brand navBrandText text-uppercase font-weight-bold" href="index.html"><img class="set-logo" src="" alt="" /></a>
+
+                                                            </div>
+                                                            <div class="col-md-9 col-sm-9 col-xs-9">
+
+                                                            </div>
+                                                        </div>
+
+
+                                                    </div>
+                                                    <div class="col-md-3 col-sm-3 col-xs-3">
+                                                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                                            <span class="sr-only">Toggle navigation</span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                            
+                                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                            <ul class="nav navbar-nav navbar-right navBar">
+                                                    <li class="nav-item"><a href="./index.php?page=home" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Home <span class="sr-only">(current)</span></a></li>
+                                                    <li class="nav-item"><a href="./index.php?page=schedule" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Bus Shedule</a></li>
+                                                    <li class="nav-item"><a href="./index.php?page=booked" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Booking</a></li>
+                                                    <li class="nav-item"><a href="#section3" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Donate</a></li>
+                                                    <li class="nav-item"><a href="reviews.html" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Review</a></li>
+                                                    <li class="nav-item"><a href="#section5" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">About</a></li>
+                                                    <li class="nav-item"><a href="#section6" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Contact</a></li>
+                                                    <li class="drop-down nav-bus nav-location"><a href="#">Maintenance</a>
+            <ul>
+              <li><a href="./index.php?page=bus">Bus List</a></li>
+              <li><a href="./index.php?page=location">Location List</a></li>
+             
+              
+            </ul>
+          </li>
+          <li class="drop-down nav-user"><a href="#"><?php echo $_SESSION['login_name'] ?> </a>
+             <ul>
+  
+              <li><a href="./index.php?page=user">Users</a></li>
+              <li><a href="javascript:void(0)" id="manage_account">Manage Account</a></li>
+              <li><a href="./logout.php">Logout</a></li>
+             
+            </ul>
+          </li>
+
+
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                </header>
+                               
+                            </div>
+						</div>
+                  </div>
+			</div>
+		</header>
+  <script>
+    $(document).ready(function(){
+      var page = '<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>';
+      if(page != ''){
+        $('#top-nav li').removeClass('active')
+        $('#top-nav li.nav-'+page).addClass('active')
+      }
+      $('#manage_account').click(function(){
+      uni_modal('Manage Account','manage_account.php')
+  })
+    })
+
+  </script>

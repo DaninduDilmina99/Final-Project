a5e8b3385594fd8abb863a91680698079887284b
Kavindu Lakshitha <Kavindulakshitha84@gmail.com>
2022-02-08 00:04:46 +0530

    changed

--- homepage.php
+++ homepage.php
@@ -1,107 +1,174 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <title>Home Page</title>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Home Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Cemre Tellioğlu Tunçay">
 
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap&subset=latin-ext" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="homepage.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/reset.css">
 
-</head>
-<body>
-   <div class="header">
-    <a href="#" class="logo"> <img src="images/Logo.png" alt=""></a>
-    <div class="navbar">
-        <div id="nav-close" class="fas fa-times"></div>  
-        <a href="./index.php?page=homepage">Home</a>
-        <a href="./index.php?page=schedule">Bus Schedule</a>
-        <a href="./index.php?page=home">Booking</a>
-        <a href="#Donate">Donate</a>
-        <a href="./index.php?page=review">Reviews</a>
-        <a href="#About">About</a>
-        <a href="#Contact">Contact</a>
-    </div>
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/responsive.css">
 
-    <div class="icon">
-        <div id="menu-btn" class="fas fa-bars"></div>
-    </div>
+        <link rel="stylesheet" href="css/new.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 
-</div>
- 
-<section class="home" id="home">
 
-    <div class="swiper home-slider">
+    </head>
+    <body>
         
+		<header class="top">
+                  <div class="fixedArea">
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12 noPadding">
+                            <div class="content-wrapper one">
+                                
+                                <header class="header">
+                                    <nav class="navbar navbar-default myNavBar">
+                                        <div class="container">
 
-        <div class="swiper-wrapper">
-            
-            <div class="swiper-slide">
+                                           
+                                            <div class="navbar-header">
+                                                <div class="row">
+                                                    <div class="col-md-9 col-sm-9 col-xs-9">
+                                                        <div class="row">
+                                                            <div class="col-md-3 col-xs-3 col-sm-3">
+                                                                <a style="padding-top:0px;" class=" navbar-brand navBrandText text-uppercase font-weight-bold" href="index.html"><img class="set-logo" src="img/logo/Logo.png" alt="restorant" /></a>
 
-                <div class="box" style="background: url('images/home-bg-1.jpg') no-repeat;">
+                                                            </div>
+                                                            <div class="col-md-9 col-sm-9 col-xs-9">
 
-                    <div class="content">
-                        <span>never stop</span>
-                        <h3>exploring</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium ab dicta incidunt optio consequuntur, doloremque quis tempora repudiandae ad ipsam tempore. Vero quis nobis id nulla ipsam placeat, iste commodi!</p>
+                                                            </div>
+                                                        </div>
 
-                        <a href="#" class="btn">get started</a>
+
+                                                    </div>
+                                                    <div class="col-md-3 col-sm-3 col-xs-3">
+                                                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                                            <span class="sr-only">Toggle navigation</span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                            
+                                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                            <ul class="nav navbar-nav navbar-right navBar">
+                                                    <li class="nav-item"><a href="./index.php?page=homepage" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Home <span class="sr-only">(current)</span></a></li>
+                                                    <li class="nav-item"><a href="./index.php?page=schedule" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Bus Shedule</a></li>
+                                                    <li class="nav-item"><a href="./index.php?page=home" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Booking</a></li>
+                                                    <li class="nav-item"><a href="#section3" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Donate</a></li>
+                                                    <li class="nav-item"><a href="./index.php?page=reviews" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Review</a></li>
+                                                    <li class="nav-item"><a href="#section5" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">About</a></li>
+                                                    <li class="nav-item"><a href="#section6" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Contact</a></li>
+
+
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                </header>
+                               
+                            </div>
+						</div>
+                  </div>
+			</div>
+		</header>
+	
+        <section id="section0" class="slider-area">
+            <div class="main-slider owl-theme owl-carousel">
+                <div class="single-slide item slider-set" style="background-image: url(img/slider/6.jpg)">
+
+                    <div class="background-dark">
+
                     </div>
-                </div>
-            </div>
+                  <div  class="slider-content-area-1">
+                    <div class="row">
+                      <div class="slide-content-wrapper text-center">
+                        <div class="slide-content">
+                          <h3>BAKERY MAKES </h3>
+                          <h2>A Taste Of  The Good Life</h2>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
 
-            <div class="swiper-slide">
-
-                <div class="box second" style="background: url(images/home-bg-3.jpg) no-repeat;">
-
-                    <div class="content">
-                        <span>make toure</span>
-                        <h3>amazing</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium ab dicta incidunt optio consequuntur, doloremque quis tempora repudiandae ad ipsam tempore. Vero quis nobis id nulla ipsam placeat, iste commodi!</p>
-
-                        <a href="#" class="btn">get started</a>
+                          <a class="default-btn1 learn-more" href="about.html">BUS SHEDULE</a>
+                        </div>
+                      </div>
                     </div>
-
+                  </div>
                 </div>
-            </div>
+                <div class="single-slide item slider-set" style="background-image: url(img/slider/2.jpeg)">
 
-            <div class="swiper-slide">
+                    
+                    <div class="background-dark">
 
-                <div class="box" style="background: url(images/home-bg-2.jpg) no-repeat;">
-
-                    <div class="content">
-                        <span>explore the</span>
-                        <h3>new world</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium ab dicta incidunt optio consequuntur, doloremque quis tempora repudiandae ad ipsam tempore. Vero quis nobis id nulla ipsam placeat, iste commodi!</p>
-
-                        <a href="#" class="btn">get started</a>
                     </div>
+                  <div  class="slider-content-area-2">
+                    <div class="row">
+                      <div class="slide-content-wrapper text-center">
+                        <div class="slide-content">
 
+                          <h3>BAKERY MAKES </h3>
+                          <h2>A Taste Of  The Good Life</h2>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
+                          <a class="default-btn2 learn-more" href="about.html">BOOKING</a>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-    </div>
-</section>
+                <div class="single-slide item slider-set" style="background-image: url(img/slider/3.jpeg)">
+
+                    <!--<img src="img/slider/bakery.jpg">-->
+                    <!-- Start Slider Content -->
+                    <div class="background-dark">
+
+                    <!-- Start Slider Content -->
+                    </div>
+                  <div  class="slider-content-area-3">
+                    <div class="row">
+                      <div class="slide-content-wrapper text-center">
+                        <div class="slide-content">
+
+                          <h3>BAKERY MAKES </h3>
+                          <h2>A Taste Of  The Good Life</h2>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
+                          <a class="default-btn3 learn-more" href="about.html" >DONATE</a>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+             </div>
+        </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-
-    <script src="homepage.js"></script>
-</body>
+        <script src="js/vendor/jquery-1.12.0.min.js"></script>
+        <script src="js/jquery-easing/jquery.easing.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/parallax.min.js"></script>
+        <script src="js/ajax-mail.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.nicescroll.min.js"></script>
+        <script src="js/main.js"></script>
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDyIMWhs-crjT0yhctbRjfJFq75FlEhSzE&callback=initMap">
+        </script>
+    </body>
 </html>
